8941efb0072214fa366f0909ca7440fc0e184d42
Rearius <arearius@gmail.com>
2018-09-27 20:05:03 +0300

    test

---
+++ controllers/siteController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rearius
+ * Date: 27.09.2018
+ * Time: 19:40
+ */
+
+echo "test";
+
+class siteController
+{
+
+}
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rearius
+ * Date: 27.09.2018
+ * Time: 19:38
+ */
+
+require_once __DIR__ . "../controllers/siteController.php";
+require_once "config.php";
+
+$site = new siteController();
+
+echo "Ok";
