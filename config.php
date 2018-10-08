1368b41a53a055e647c53f17af162cdd14dd1114
Rearius <arearius@gmail.com>
2018-10-08 20:17:09 +0300

    Empty MVC + Logger. Need to fix work with db

--- config.php
+++ config.php
@@ -1,7 +1,16 @@
-<?php
+<?
 /**
  * Created by PhpStorm.
  * User: Rearius
- * Date: 27.09.2018
- * Time: 19:38
+ * Date: 05.10.2018
+ * Time: 22:30
  */
+
+//Настройки подключения к базе MySQl
+define("sql_user", "root");//Логин
+define("sql_host", "localhost");//Адрес сервера
+define("sql_pass", "");//Пароль
+define("sql_table", "loumvc");//Таблица базы данных
+define("title", "Лёгкий MVC");//Базовый тайтл
+
+?>
--- index.php
+++ index.php
@@ -2,13 +2,43 @@
 /**
  * Created by PhpStorm.
  * User: Rearius
- * Date: 27.09.2018
- * Time: 19:38
+ * Date: 05.10.2018
+ * Time: 21:50
  */
 
-require_once __DIR__ . "../controllers/siteController.php";
-require_once "config.php";
+$data = explode('/' ,$_REQUEST['uri']);
+$_REQUEST['controller'] = $data[1];
+$_REQUEST['action'] = $data[2];
+//print_r($data);
 
-$site = new siteController();
+//print_r($_GET);
+//print_r($_REQUEST);
 
-echo "Ok";
+//echo "test mvc server";
+//echo "</pre>";
+//echo $_SERVER['DOCUMENT_ROOT'];
+
+//Подключаем настройки проекта
+include $_SERVER['DOCUMENT_ROOT']. "/config.php";
+//Загружаем базовые функции
+//include $_SERVER['DOCUMENT_ROOT']."/model/functions.php";
+//Загружаем класс для работы с базой данных
+include $_SERVER['DOCUMENT_ROOT']."/model/db.php";
+//Загружаем реестр
+include $_SERVER['DOCUMENT_ROOT'] . "/core/app.php";
+//Включение автозагрузки
+include $_SERVER['DOCUMENT_ROOT'] . "/core/autoload.php";
+
+//$logger = new Logger();
+//$logger->log("index.php start");
+
+//Сохранение данных запроса
+//echo "<br>Getting controller<br>";
+App::get('AppController')->setRequest($_REQUEST);
+//echo "<br>Getting controller2<br>";
+if ($type == 'ajax') {
+    App::get('AppController')->ajax($_REQUEST['controller'], $_REQUEST['action'], $_REQUEST['send']);
+} else {
+    //echo "Getting controller app ";
+    App::get('AppController')->page($_REQUEST['controller'], $_REQUEST['action'],$_REQUEST);
+}
